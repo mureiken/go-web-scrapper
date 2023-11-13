e48c77c7632a13bbab735e264577264f88989c6f
Ken Murei <mureiken@gmail.com>
2023-11-13 11:23:37 +0300

    Web scrapper logic using Colly

---
+++ scraper.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/gocolly/colly"
+)
+
+type PokemonProduct struct {
+	url, image, name, price string
+}
+
+func main() {
+	var pokemonProducts []PokemonProduct
+
+	c := colly.NewCollector()
+
+	// scraping logic 
+	c.OnHTML("li.product", func(e *colly.HTMLElement) {
+		pokemonProduct := PokemonProduct{}
+
+		pokemonProduct.url = e.ChildAttr("a", "href")
+		pokemonProduct.image = e.ChildAttr("img", "src")
+		pokemonProduct.name = e.ChildText("h2")
+		pokemonProduct.price = e.ChildText(".price")
+
+		pokemonProducts = append(pokemonProducts, pokemonProduct)
+	})
+
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL)
+	})
+
+	c.Visit("https://scrapeme.live/shop/")
+
+	// opening the CSV file
+	file, err := os.Create("products.csv")
+	if err != nil {
+		log.Fatalln("Failed to create output CSV file", err)
+	}
+	defer file.Close()
+
+	// initializing a file writer
+	writer := csv.NewWriter(file)
+
+	// writing the CSV headers
+	headers := []string{
+		"url",
+		"image",
+		"name",
+		"price",
+	}
+	writer.Write(headers)
+
+	// writing each Pokemon product as a CSV row
+	for _, pokemonProduct := range pokemonProducts {
+		// converting a PokemonProduct to an array of strings
+		record := []string{
+			pokemonProduct.url,
+			pokemonProduct.image,
+			pokemonProduct.name,
+			pokemonProduct.price,
+		}
+
+		// adding a CSV record to the output file
+		writer.Write(record)
+	}
+	defer writer.Flush()
+}
